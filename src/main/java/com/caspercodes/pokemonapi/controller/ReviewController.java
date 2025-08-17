bc6365badeda6da346fc8c87ab8e5ecadb4468d8
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-08-17 18:21:04 +0100

    Building review controller

--- src/main/java/com/caspercodes/pokemonapi/controller/ReviewController.java
+++ src/main/java/com/caspercodes/pokemonapi/controller/ReviewController.java
@@ -1,4 +1,37 @@
 package com.caspercodes.pokemonapi.controller;
 
+import com.caspercodes.pokemonapi.dto.ReviewDtoResponse;
+import com.caspercodes.pokemonapi.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/")
 public class ReviewController {
+    private final ReviewService reviewService;
+
+    @PostMapping("pokemon/{pokemonId}/reviews")
+    public ResponseEntity<ReviewDtoResponse> createReview(@PathVariable(value = "pokemonId") Long pokemonId, @RequestBody ReviewDtoResponse reviewDtoResponse) {
+        return new ResponseEntity<>(reviewService.createReview(pokemonId, reviewDtoResponse), HttpStatus.CREATED);
+    }
+
+    @GetMapping("pokemon/{pokemonId}/reviews")
+    public ResponseEntity<List<ReviewDtoResponse>> getReviewsByPokemonId(@PathVariable(value = "pokemonId") Long pokemonId) {
+        return new ResponseEntity<>(reviewService.getReviewByPokemonId(pokemonId), HttpStatus.OK);
+    }
+
+    @GetMapping("pokemon/{pokemonId}/reviews/{reviewId}")
+    public ResponseEntity<ReviewDtoResponse> getReviewsById(@PathVariable(value = "pokemonId") Long pokemonId, @PathVariable(value = "reviewId") Long reviewId) {
+        return new ResponseEntity<>(reviewService.getReviewById(pokemonId, reviewId), HttpStatus.OK);
+    }
+
+    @PutMapping("pokemon/{pokemonId}/reviews/{reviewId}")
+    public ResponseEntity<ReviewDtoResponse> updateReview(@PathVariable(value = "pokemonId") Long pokemonId, @PathVariable(value = "reviewId") Long reviewId, ReviewDtoResponse reviewDtoResponse) {
+        return new ResponseEntity<>(reviewService.updateReview(pokemonId, reviewId, reviewDtoResponse), HttpStatus.OK);
+    }
 }

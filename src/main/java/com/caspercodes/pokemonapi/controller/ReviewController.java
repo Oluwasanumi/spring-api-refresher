19602cbbf653c125326339d9f43334b31f7a1b9c
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-08-17 18:24:47 +0100

    Delete mapping

--- src/main/java/com/caspercodes/pokemonapi/controller/ReviewController.java
+++ src/main/java/com/caspercodes/pokemonapi/controller/ReviewController.java
@@ -16,7 +16,8 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("pokemon/{pokemonId}/reviews")
-    public ResponseEntity<ReviewDtoResponse> createReview(@PathVariable(value = "pokemonId") Long pokemonId, @RequestBody ReviewDtoResponse reviewDtoResponse) {
+    public ResponseEntity<ReviewDtoResponse> createReview(@PathVariable(value = "pokemonId") Long pokemonId,
+                                                          @RequestBody ReviewDtoResponse reviewDtoResponse) {
         return new ResponseEntity<>(reviewService.createReview(pokemonId, reviewDtoResponse), HttpStatus.CREATED);
     }
 
@@ -26,12 +27,22 @@ public class ReviewController {
     }
 
     @GetMapping("pokemon/{pokemonId}/reviews/{reviewId}")
-    public ResponseEntity<ReviewDtoResponse> getReviewsById(@PathVariable(value = "pokemonId") Long pokemonId, @PathVariable(value = "reviewId") Long reviewId) {
+    public ResponseEntity<ReviewDtoResponse> getReviewsById(@PathVariable(value = "pokemonId") Long pokemonId,
+                                                            @PathVariable(value = "reviewId") Long reviewId) {
         return new ResponseEntity<>(reviewService.getReviewById(pokemonId, reviewId), HttpStatus.OK);
     }
 
     @PutMapping("pokemon/{pokemonId}/reviews/{reviewId}")
-    public ResponseEntity<ReviewDtoResponse> updateReview(@PathVariable(value = "pokemonId") Long pokemonId, @PathVariable(value = "reviewId") Long reviewId, ReviewDtoResponse reviewDtoResponse) {
+    public ResponseEntity<ReviewDtoResponse> updateReview(@PathVariable(value = "pokemonId") Long pokemonId,
+                                                          @PathVariable(value = "reviewId") Long reviewId,
+                                                          ReviewDtoResponse reviewDtoResponse) {
         return new ResponseEntity<>(reviewService.updateReview(pokemonId, reviewId, reviewDtoResponse), HttpStatus.OK);
+    }
+
+    @DeleteMapping("pokemon/{pokemonId}/reviews/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable(value = "pokemonId") Long pokemonId,
+                                               @PathVariable(value = "reviewId") Long reviewId) {
+        reviewService.deleteReview(pokemonId, reviewId);
+        return new ResponseEntity<>("Review with id: " + reviewId + " for Pokemon with id: " + pokemonId + " has been deleted successfully", HttpStatus.OK);
     }
 }
